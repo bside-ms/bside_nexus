8b5ba22a17227f7f9a7fb68f2e558b79924f4240
Carsten Dietzel <carsten.dietzel@check24.de>
2024-09-01 11:30:32 +0200

    Add startpage link

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import type { ReactElement } from 'react';
 import { SiNexusmods } from 'react-icons/si';
 import GroupsNavigation from '@/components/navigation/GroupsNavigation';
@@ -9,9 +10,11 @@ const Navigation = async (): Promise<ReactElement> => {
     return (
         <aside className="w-64 border-r border-gray-200 bg-white">
             <div className="p-5">
-                <h2 className="text-xl">
-                    B-Side Nexus <SiNexusmods className="inline" />
-                </h2>
+                <Link href="/">
+                    <h2 className="text-xl">
+                        B-Side Nexus <SiNexusmods className="inline" />
+                    </h2>
+                </Link>
             </div>
 
             {isLoggedIn && (
