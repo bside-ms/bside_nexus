49b08120e76fa23e991aab628e2e40f9455650d1
Kennet <kennet@seebruecke.org>
2025-01-13 20:41:28 +0100

    Rudimentary Group Details

--- app/groups/[id]/page.tsx
+++ app/groups/[id]/page.tsx
@@ -1,23 +1,57 @@
 import { redirect } from 'next/navigation';
 import type { ReactElement } from 'react';
-import GroupInfo from '@/components/group/GroupInfo';
 import GroupMembers from '@/components/group/GroupMembers';
+import NavbarTop from '@/components/sidebar/NavbarTop';
+import { Separator } from '@/components/ui/separator';
 import { getGroupById, getGroupMembers } from '@/lib/keycloak/groupActions';
+
+export function generateStaticParams(): Array<{ id: string }> {
+    // ToDo: Replace with real function.
+    const groupIds = ['844eacda-e143-4012-b7d2-d2fe2639101e', 'c5df5b4e-d702-423b-9d52-2001109d6941'];
+    return groupIds.map((id) => ({ id }));
+}
+
+export const revalidate = 300;
 
 const Group = async ({ params: { id: groupId } }: { params: { id: string } }): Promise<ReactElement> => {
     const group = await getGroupById(groupId);
 
+    // Handle when the group is not found
     if (group === null) {
-        redirect('/');
+        redirect('/groups');
     }
+
+    const breadCrumbs = [
+        {
+            title: 'Meine Gruppen',
+            url: `/groups`,
+        },
+        {
+            title: group.attributes.categoryName ?? '',
+        },
+        {
+            title: group.attributes.displayName ?? '',
+            active: true,
+        },
+    ];
 
     const [membersGroup, groupMembers] = await getGroupMembers(group);
 
     return (
-        <div className="space-y-8">
-            <GroupInfo group={group} />
+        <div className="">
+            <NavbarTop items={breadCrumbs} />
+            <div className="space-y-6 p-10 pb-16">
+                <div className="space-y-0.5">
+                    <h2 className="text-2xl font-bold tracking-tight">Gruppenverwaltung: {group.attributes.displayName ?? group.name}</h2>
+                    <p>Hier findest du eine Übersicht über alle Gruppen auf die du innerhalb der B-Side zugreifen kannst.</p>
+                </div>
 
-            {membersGroup !== null && groupMembers.length > 0 && <GroupMembers group={membersGroup} members={groupMembers} />}
+                <Separator className="my-6" />
+
+                <div className="space-y-8">
+                    {membersGroup !== null && groupMembers.length > 0 && <GroupMembers members={groupMembers} />}
+                </div>
+            </div>
         </div>
     );
 };
--- components/group/GroupMembers.tsx
+++ components/group/GroupMembers.tsx
@@ -1,20 +1,10 @@
 import type { ReactElement } from 'react';
 import GroupMembersTable from '@/components/group/GroupMembersTable';
-import type { AugmentedGroupRepresentation } from '@/lib/keycloak/groupActions';
-import { getGroupName } from '@/lib/keycloak/groupActions';
 import type { AugmentedUserRepresentation } from '@/lib/keycloak/userActions';
 
-const GroupMembers = ({
-    group,
-    members,
-}: {
-    group: AugmentedGroupRepresentation;
-    members: Array<AugmentedUserRepresentation>;
-}): ReactElement => {
+const GroupMembers = ({ members }: { members: Array<AugmentedUserRepresentation> }): ReactElement => {
     return (
         <div className="px-3">
-            <h2 className="text-lg">{getGroupName(group)}</h2>
-
             <GroupMembersTable members={members} />
         </div>
     );
