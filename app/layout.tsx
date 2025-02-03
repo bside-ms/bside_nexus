4ee05c41e4f8b90c66cfaf17485633ffaf0702c2
Kennet <kennet@seebruecke.org>
2025-02-03 12:59:15 +0100

    Login-Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css';
+import '@/app/globals.css';
 
 import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
 import type { ReactElement, ReactNode } from 'react';
-import Login from '@/components/Login';
+import { LoginForm } from '@/components/auth/LoginForm';
 import { AppSidebar } from '@/components/sidebar/AppSidebar';
 import ThemeProvider from '@/components/theming/ThemeProvider';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
@@ -22,7 +22,7 @@ const RootLayout = async ({ children }: Readonly<{ children: ReactNode }>): Prom
                 <body className={GeistSans.className}>
                     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
                         <main className="">
-                            <Login />
+                            <LoginForm />
                         </main>
                     </ThemeProvider>
                 </body>
@@ -37,7 +37,7 @@ const RootLayout = async ({ children }: Readonly<{ children: ReactNode }>): Prom
                     <SidebarProvider>
                         <AppSidebar />
                         <SidebarInset>
-                            <main className="">{isLoggedIn ? children : <Login />}</main>
+                            <main className="">{children}</main>
                         </SidebarInset>
                     </SidebarProvider>
                 </ThemeProvider>
---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useCallback } from 'react';
+import { signIn } from 'next-auth/react';
+import type { ReactElement } from 'react';
+import { GrLogin } from 'react-icons/gr';
+import { ThemeToggle } from '@/components/theming/ThemeToggle';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+export function LoginForm(): ReactElement {
+    const login = useCallback(() => signIn('keycloak'), []);
+
+    return (
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <a href="#" className="flex items-center gap-2 font-medium">
+                        B-Side
+                        <small className="px-4">Interner Bereich</small>
+                    </a>
+                    <div className="ml-auto">
+                        <ThemeToggle />
+                    </div>
+                </div>
+                <div className="" />
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <div className={cn('flex flex-col gap-6')}>
+                            <div className="flex flex-col items-center gap-2 text-center">
+                                <h1 className="text-2xl font-bold">Willkommen zurück!</h1>
+                                <p className="text-muted-foreground text-balance text-sm">
+                                    Hier kannst du dich mit deinem B-Side Account einloggen.
+                                </p>
+                            </div>
+                            <div className="grid gap-6">
+                                <Button variant="secondary" className="w-full" onClick={login}>
+                                    <GrLogin />
+                                    Login with B-Side
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="relative hidden lg:block">
+                <img src="/login.jpeg" alt="Image" className="absolute inset-0 size-full object-cover dark:brightness-[0.75]" />
+            </div>
+        </div>
+    );
+}
